63c44f540aa9ad9fd5688e783a12db8eb2c40ded
hunterb8 <brittan.hunter@gmail.com>
2022-03-17 06:54:51 -0600

    initial

---
+++ Bookstore/Migrations/20220313104054_Initial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookstore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+       
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartLineItem");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Purchases");
+        }
+    }
+}
---
+++ Bookstore/Models/IBookstoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+    }
+}
